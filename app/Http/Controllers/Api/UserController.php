78d72b0d9aadd925f04e5d7886bc8e620d4ac4a3
Snuvbch <alvinfandi43@gmail.com>
2023-06-16 16:54:35 +0700

    ckc

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -9,6 +9,7 @@ use Illuminate\Http\Request;
 use App\Helpers\ResponseFormatter;
 use Exception;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 use Laravel\Fortify\Rules\Password;
 
 
@@ -16,113 +17,28 @@ class UserController extends Controller
 {  
     public function register(Request $request)
     {
-        try {
-            $validator = Validator::make($request->all(), [
-                'name' => ['required', 'string', 'max:100'],
-                'username' => ['required', 'string', 'max:100', 'unique:users'],
-                'email' => ['required', 'string', 'max:255', 'email', 'unique:users'],
-                'password' => ['required', 'string', new Password],
-                'confirm_password' => ['required'],
-                'birthdate' => ['string'],
-                'is_checked' => ['required'],
-                'phone_number' => ['required', 'numeric', 'unique:users'],
-            ]);
-    
-            if ($validator->fails()) {
-                return ResponseFormatter::error([
-                    'message' => 'Bad Request',
-                    'errors' => $validator->errors()
-                ], 'Bad Request', 400);
-            }
-
-            if ($request->confirm_password != $request->password){
-                return ResponseFormatter::error([
-                    'message' => 'Bad Request',
-                    'errors' => $validator->errors()->add('confirm_password', 'confirmation password must be the same as the password'),
-                ], 'Bad Request', 400);
-            }
-
-            if ($request->is_checked == "false"){
-                return ResponseFormatter::error([
-                    'message' => 'Bad Request',
-                    'errors' => $validator->errors()->add('is_checked', 'terms of service & privacy policy must be agree'),
-                ], 'Bad Request', 400);
-            }
-
-            User::create([
-                'name' => $request->name,
-                'username' => $request->username,
-                'email' => $request->email,
-                'birthdate' => $request->birthdate,
-                'phone_number'=> $request->phone_number,
-                'password' => Hash::make($request->password),
-            ]);
-
-            $user = User::where('email', $request->email)->first();
-
-            $token = $user->createToken('authToken')->plainTextToken;
-
-            return ResponseFormatter::success([
-                'access_token' => $token,
-                'token_type' => env('TOKEN_TYPE', 'secret'),
-                'user' => $user
-            ], "user registered");
-        } catch (Exception $error) {
-            return ResponseFormatter::error([
-                "message" => " something erorr",
-                "error" => $error
-            ], 'authentication failed', 500);
-        }
-    }
-
-    public function getAllUsers()
-    {   
-        try {
-            $user = User::all();
-            
-            return ResponseFormatter::success([
-                'user' => $user
-            ], "success get all users");
-        } catch (Exception $error) {
-            return ResponseFormatter::error([
-                "message" => " something erorr",
-                "error" => $error
-            ], 'authentication failed', 500);
-        }
-    }
-
-    public function getUserById($id)
-    {   
-        try {
-            $user = User::find($id);
-            
-            if ($user) {
-                return ResponseFormatter::success([
-                    'user' => $user
-                ], "success get user");
-            }
-
-            return ResponseFormatter::error([
-                'message' => 'not found',
-            ], 'user not found', 404);
-            
-            
-        } catch (Exception $error) {
-            return ResponseFormatter::error([
-                "message" => " something erorr",
-                "error" => $error
-            ], 'authentication failed', 500);
-        }
-    }
-
-    public function login(Request $request)
-    {
-        $validator = Validator::make($request->all(),[
-            'email' => ['required', 'email'],
-            'password' => ['required'],
+        //validasi
+        $validator = Validator::make($request->all(), [
+            'name' => ['required', 'string', 'max:100'],
+            'username' => ['required', 'string', 'max:100', 'unique:users'],
+            'email' => ['required', 'string', 'max:255', 'email', 'unique:users'],
+            'password' => ['required', 'string', 'min:8'],
+            'phone_number' => ['required', 'numeric', 'unique:users'],
+            'birthdate' => ['date'],
+            'bio' => ['string', 'max:500'],
+            'location' => ['string'],
+            'job_status' => ['string'],
         ],[
+            'name.required' => 'Nama harus diisi.',
+            'username.unique' => 'Username sudah dipakai.',
+            'username.required' => 'Username harus diisi.',
             'email.required' => 'Email harus diisi.',
+            'email.email' => 'Format email tidak valid.',
+            'email.unique' => 'Email sudah terdaftar.',
             'password.required' => 'Password harus diisi.',
+            'password.confirmed' => 'Password tidak sama.',
+            'phone_number.required' => 'Nomor telepon harus diisi.',
+            'phone_number.unique' => 'Nomor telepon sudah diisi. ',
         ]);
 
         if ($validator->fails()){
@@ -132,24 +48,71 @@ class UserController extends Controller
                 'message' => $validator->errors()
             ], 400);
         }else{
-            //jika ok
-            if (Auth::attempt(['email' => $request->email, 'password'=> $request->password])) {
-                //jika username atau password valid
-                $user = Auth::user(); 
-                $token = $user->createToken('authToken')->plainTextToken;
+            //jika ok, simpan user baru
+            $user = new User();
+            $user->name = $request->name;
+            $user->username = $request->username;
+            $user->birthdate = $request->birthdate;
+            $user->email = $request->email;
+            $user->password = Hash::make($request->password);
+            $user->phone_number = $request->phone_number;
+            $user->save();
 
-                return response()->json([
-                    'status' => true,
-                    'message' => 'Login berhasil.',
-                    'token' => $token
-                ], 200);
-            }else{
-                //jika username atau password tidak valid
-                return response()->json([
-                    'status' => false,
-                    'message' => 'Login gagal.'
-                ], 400);
+            return response()->json([
+                'status' => true,
+                'message' => 'User registered.'
+            ],201);
+        }
+    }
+
+    // Metode lain di sini...
+    public function login(Request $request)
+    {
+        try {
+            $validator = Validator::make($request->all(), [
+                'username' => ['required', 'string'],
+                'email' => ['required', 'string', 'email'],
+                'phone_number' => ['required', 'string'],
+                'password' => ['required', 'string'],
+            ]);
+    
+            if ($validator->fails()) {
+                return ResponseFormatter::error([
+                    'message' => 'Bad Request',
+                    'errors' => $validator->errors()
+                ], 'Bad Request', 400);
             }
+    
+            $user = User::where(function ($query) use ($request) {
+            $query->where('username', $request->input('username'))
+                    ->orWhere('email', $request->input('email'))
+                    ->orWhere('phone_number', $request->input('phone_number'));
+    })->first();
+    
+            if (!$user) {
+                return ResponseFormatter::error([
+                    'message' => 'User not found',
+                    ], 'Authentication failed', 404);
+            }
+    
+            if (!Hash::check($request->input('password'), $user->password)) {
+                return ResponseFormatter::error([
+                    'message' => 'Invalid password',
+                ], 'Authentication failed', 401);
+            }
+    
+            $token = $user->createToken('authToken')->plainTextToken;
+    
+            return ResponseFormatter::success([
+                'access_token' => $token,
+                'token_type' => env('TOKEN_TYPE', 'Bearer'),
+                'user' => $user
+            ], "Login successful");
+        } catch (Exception $error) {
+            return ResponseFormatter::error([
+                "message" => "Something error",
+                "error" => $error
+            ], 'Authentication failed', 500);
         }
     }
 }
